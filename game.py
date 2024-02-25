4d601129c4e498ade40d8f153d059324381091b1
Eduardo <eduardopoleo@gmail.com>
2024-02-25 13:09:03 -0500

    Abstracted obstacle into their own class and added flies

--- game.py
+++ game.py
@@ -1,5 +1,10 @@
+# TODO:
+# make the snail move to the left 
+# Add jump and gravity
+
 import pygame
 import sys
+import random
 
 FPS = 60
 
@@ -13,25 +18,71 @@ ground_img = pygame.image.load('graphics/Ground.png').convert()
 sky_img = pygame.image.load('graphics/Sky.png').convert()
 
 # Player image
-player_img = pygame.image.load('graphics/Player/player_stand.png').convert_alpha()
-player_rect = player_img.get_rect(x=40, y=216)
+player_img = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+# mid of the picture is at 80
+# bottom of the pic  is at 300 
+# this makes the calculations taking into account the image sizes.
+player_rect = player_img.get_rect(midbottom=(80, 300))
 
-# Snail image
-snail_img = pygame.image.load('graphics/snail/snail1.png')
-snail_rect = player_img.get_rect(x=700, y=264)
+# Snail
+SNAIL_SPEED = 2
+SPAWN_INTERVAL = 2 # spawn an enemy at every interval
+snail_img = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+
+# Flies
+OBSTACLE_SPEED = 2
+fly_img = pygame.image.load('graphics/Fly/Fly1.png').convert_alpha()
+
+obstacles = []
 
 clock = pygame.time.Clock()
+
+current_time = pygame.time.get_ticks() / 1000
+initial_time = pygame.time.get_ticks() / 1000
+
+class Obstacle:
+    def __init__(self, img, screen, height):
+        self.img = img
+        self.screen = screen
+        self.rect = img.get_rect(midbottom=(random.randint(800, 950), height))
+
+    def update(self):
+        self.rect.x -= OBSTACLE_SPEED
+
+    def render(self):
+        self.screen.blit(self.img, self.rect)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
+    
     screen.blit(sky_img, (0, 0))
     screen.blit(ground_img, (0, 300))
-    screen.blit(player_img, player_rect)
-    screen.blit(snail_img, snail_rect)
+    # screen.blit(player_img, player_rect)
 
+    current_time = pygame.time.get_ticks() / 1000
+    time_elapsed = current_time - initial_time
+
+    if time_elapsed >= SPAWN_INTERVAL:
+        initial_time = current_time
+        enemy_type = random.choice(['fly', 'snail'])
+        new_obstacle = None
+        if enemy_type == 'snail':
+            new_obstacle = Obstacle(snail_img, screen, 300)
+        else:
+            new_obstacle = Obstacle(fly_img, screen, 200)
+        
+        obstacles.append(new_obstacle)
+
+    for obstacle in obstacles:
+        if obstacle.rect.x < -200:
+            obstacles.remove(obstacle)
+        else:
+            obstacle.update()
+            obstacle.render()
+            
+    print(len(obstacles))
     pygame.display.update()
 
     clock.tick(FPS)
