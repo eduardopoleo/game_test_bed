a5ed2fc91629db9a46742e69c4b063ab569605f4
Eduardo <eduardopoleo@gmail.com>
2024-02-24 20:54:06 -0500

    Adding player and snail images using rect positioning

--- game.py
+++ game.py
@@ -8,8 +8,17 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Jumper Game")
 
+# Background
 ground_img = pygame.image.load('graphics/Ground.png').convert()
 sky_img = pygame.image.load('graphics/Sky.png').convert()
+
+# Player image
+player_img = pygame.image.load('graphics/Player/player_stand.png').convert_alpha()
+player_rect = player_img.get_rect(x=40, y=216)
+
+# Snail image
+snail_img = pygame.image.load('graphics/snail/snail1.png')
+snail_rect = player_img.get_rect(x=700, y=264)
 
 clock = pygame.time.Clock()
 
@@ -20,6 +29,9 @@ while True:
 
     screen.blit(sky_img, (0, 0))
     screen.blit(ground_img, (0, 300))
+    screen.blit(player_img, player_rect)
+    screen.blit(snail_img, snail_rect)
+
     pygame.display.update()
 
     clock.tick(FPS)
